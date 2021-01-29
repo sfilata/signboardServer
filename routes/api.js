eef081865b2a4dac378f495e99eb93a035833bcf
shu-baiqiao <shu.baiqiao@navercorp.com>
2021-01-29 17:30:57 +0800

    Feat: Add the connection to the mongoDB in remote server

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
-let toJSON = require('../utils')
-let data = require('../data/modelList.json')
+const toJSON = require('../utils')
+const data = require('../data/modelList.json')
+const MongoClient = require('mongodb').MongoClient;
 
 /* GET api listing. */
 router.get('/modelList', function(req, res, next) {
-  res.json(toJSON(data));
+  MongoClient.connect('mongodb://neil:13579@139.9.172.252:27017/liveDB?authSource=user-data', function (err, client) {
+    if (err) throw err
+
+    var db = client.db('liveDB')
+
+    db.collection('modelList').find().toArray((err, result) => {
+      if (err) throw err
+
+      console.log(result)
+      res.json(toJSON(result));
+    })
+  })
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  mongoose.connect('mongodb://139.9.172.252:27017/liveDB');
+
+  const conn = mongoose.connection;
+  conn.on('connected', (err) => {
+    if (err) throw err
+    console.log('The connection is successfully established');
+  })
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
