7623fdcc080f91b65f369cfcfa3e6a31190c80f8
shu-baiqiao <shu.baiqiao@navercorp.com>
2021-02-01 16:55:44 +0800

    Feat: Support to transfer the file through api.

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,49 @@
 var express = require('express');
+let path = require('path');
 var router = express.Router();
 const toJSON = require('../utils')
-const data = require('../data/modelList.json')
 const MongoClient = require('mongodb').MongoClient;
+var fs = require('fs');
+
+/** For Local Debugging */
+var userData = JSON.parse(fs.readFileSync('config.json'));
 
 /* GET api listing. */
-router.get('/modelList', function(req, res, next) {
-  MongoClient.connect('mongodb://neil:13579@139.9.172.252:27017/liveDB?authSource=user-data', function (err, client) {
+router.get('/modelList/:name', function(req, res, next) {
+  const { user, password, ip, port } = userData;
+  MongoClient.connect(`mongodb://${user}:${password}@${ip}:${port}/liveDB?authSource=user-data`, function (err, client) {
     if (err) throw err
 
     var db = client.db('liveDB')
 
-    db.collection('modelList').find().toArray((err, result) => {
+    db.collection('modelList').find({name: req.params.name}).toArray((err, result) => {
       if (err) throw err
 
       console.log(result)
       res.json(toJSON(result));
     })
+  })
+});
+
+router.get('/model/:fileName', function(req, res, next) {
+  
+  const option = {
+    root: path.join(__dirname, '../public/images/'),
+    dotfiles: 'allow',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
+  console.log(option)
+
+  const fileName = req.params.fileName;
+  res.sendFile(fileName, option, function (err) {
+    if (err) {
+      next(err)
+    } else {
+      console.log('Sent:', fileName)
+    }
   })
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  mongoose.connect('mongodb://139.9.172.252:27017/liveDB');
-
-  const conn = mongoose.connection;
-  conn.on('connected', (err) => {
-    if (err) throw err
-    console.log('The connection is successfully established');
-  })
-  // res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
